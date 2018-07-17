6eab3cee01cb3452b6334e89dab8cc17e9f13eac
pramod-android <40615535+pramod-android@users.noreply.github.com>
2018-07-17 06:24:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/win/fragmentimplementationdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.win.fragmentimplementationdemo;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements FragmentActionListener{
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        addCountriesFragment();
+    }
+
+    private void addCountriesFragment(){
+        fragmentTransaction=fragmentManager.beginTransaction();
+
+        CountriesFragment countryListFragment=new CountriesFragment();
+        countryListFragment.setFragmentActionListener(this);
+
+        fragmentTransaction.add(R.id.fragmentContainer,countryListFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void addCountryDescriptionFragment(String countryName){
+        fragmentTransaction=fragmentManager.beginTransaction();
+
+        CountryDescriptionFragment countryDescriptionFragment=new CountryDescriptionFragment();
+
+        Bundle bundle=new Bundle();
+        bundle.putString(FragmentActionListener.KEY_SELECTED_COUNTRY,countryName);
+        countryDescriptionFragment.setArguments(bundle);
+
+        fragmentTransaction.replace(R.id.fragmentContainer,countryDescriptionFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onCountrySelected(String country) {
+        addCountryDescriptionFragment(country);
+    }
+}
